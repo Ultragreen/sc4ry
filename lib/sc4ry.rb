357e02751e88c8a65ccaa9113c00766a31d5a760
= <=>
2022-02-20 23:27:52 +0100

    adding logs and notifiers

--- lib/sc4ry.rb
+++ lib/sc4ry.rb
@@ -1,30 +1,17 @@
 #require "sc4ry/version"
 
 require 'rest-client'
-
-
+require 'prometheus/client'
+require 'prometheus/client/push'
 
 require 'logger' 
 require 'timeout'
 require 'forwardable'
 require 'singleton'
-
-
-
-
+require 'socket'
 
 module Sc4ry
-
-
- 
-
-
-
-   
-
   module Backends
-
-
 
     class Memory
       def initialize(config=nil?)
@@ -54,8 +41,6 @@ module Sc4ry
       def exist?(options)
         return @data.include? options[:key]
       end
-
-
 
     end
 
@@ -146,7 +131,7 @@ module Sc4ry
 
     def change_backend(options)
       @@current = options[:name]
-      @be = @@backends[@@current][:class]::new 
+      @be = @@backends[@@current][:class]::new(@@backends[@@current][:config])
     end
 
     def register_backend(options)
@@ -165,11 +150,109 @@ module Sc4ry
   end
 
 
+  module Helpers
 
+    def Helpers.log(options)
+      Sc4ry::Logger.current = options[:target] if options[:target]
+      Sc4ry::Logger.get.send options[:level], "Sc4ry : #{options[:message]}"  
+    end
+
+    # TCP/IP service checker
+    # @return [Bool] status
+    # @param [Hash] options
+    # @option options [String] :host hostname
+    # @option options [String] :port TCP port
+    # @option options [String] :url full URL, priority on :host and :port
+    def Helpers.verify_service(options ={})
+      begin
+        if options[:url] then
+          uri = URI.parse(options[:url])
+          host = uri.host
+          port = uri.port
+        else
+          host = options[:host]
+          port = options[:port]
+        end
+        Timeout::timeout(1) do
+          begin
+            s = TCPSocket.new(host, port)
+            s.close
+            return true
+          rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+            return false
+          end
+        end
+      rescue Timeout::Error
+        return false
+      end
+    end
+
+
+    def Helpers.notify(options = {})
+      Sc4ry::Notifiers.list.each do |record|
+        notifier = Sc4ry::Notifiers.get name: record
+        notifier[:class].notify(options) if options[:config][:notifiers].include? record 
+      end
+    end
+
+  end 
 
 
   module Notifiers
+
+  
+    class Prometheus
+
+      @@registry = ::Prometheus::Client::Registry::new
+      @@metric_circuit_state = ::Prometheus::Client::Gauge.new(:cuircuit_state, docstring: 'Sc4ry metric : state of a circuit', labels: [:circuit])
+
+      @@registry.register(@@metric_circuit_state)
+
     
+
+      # send metrics to Prometheus PushGateway
+      # @return [Bool]
+      def Prometheus.notify(options = {})
+        @config = Sc4ry::Notifiers.get({name: :prometheus})[:config]
+        status = options[:config][:status][:general]
+        circuit = options[:circuit]
+        status_map = {:open => 0, :half_open => 1, :closed => 2}
+        if Sc4ry::Helpers::verify_service url: @config[:url] then
+          
+          @@metric_circuit_state.set(status_map[status], labels: {circuit: circuit.to_s })
+          Sc4ry::Helpers.log level: :debug, message: "Prometheus Notifier : notifying for circuit #{circuit.to_s}, state : #{status.to_s}."
+
+          return ::Prometheus::Client::Push.new(job: "Sc4ry", instance: Socket.gethostname, gateway: @config[:url]).add(@@registry)
+        else
+          Sc4ry::Helpers.log level: :warning, message: "Prometheus Notifier : can't notify Push Gateway not reachable."
+        end
+      end
+    end
+
+    @@notifiers_list = {:prometheus => {:class => Sc4ry::Notifiers::Prometheus, :config => {:url => 'http://localhost:9091'}}}
+
+    def Notifiers.list
+      return @@notifiers_list.keys
+    end
+
+    def Notifiers.get(options ={})
+      return @@notifiers_list[options[:name]]
+    end
+
+    def Notifiers.register(options)
+      raise ":name is mandatory" unless options[:name]
+      raise ":definition is mandatory" unless options[:definition]
+      @@notifiers_list[options[:name]] = options[:definition]
+    end
+
+    def Notifiers.config(options)
+      raise ":name is mandatory" unless options[:name]
+      raise ":config is mandatory" unless options[:config]
+      @@notifiers_list[options[:name]][:config] = options[:config]
+    end
+
+    
+
   end
 
 
@@ -268,6 +351,8 @@ module Sc4ry
 
   class Circuits
 
+      include Sc4ry::Helpers
+
       @@circuits_store = Sc4ry::Store.instance 
 
       @@config = { :max_failure_count => 5,
@@ -277,6 +362,7 @@ module Sc4ry
                    :max_time => 10,
                    :max_overtime_count => 3,
                    :check_delay => 30,
+                   :notifiers => [:prometheus],
                    :exceptions => [StandardError, RuntimeError]
                    }
 
@@ -319,16 +405,11 @@ module Sc4ry
           controller = Sc4ry::RunController.new(circuit)
           Circuits.control circuit: circuit_name, values: controller.run(block: block)
         end
-        Circuits.log_iteration circuit: circuit_name
+        Sc4ry::Helpers.log level: :info, message: "Circuit #{circuit_name} : status #{circuit[:status]}"
+
       end
 
       private
-
-      def Circuits.log_iteration(options={})
-        data = @@circuits_store.get key: options[:circuit]
-        Sc4ry::Logger.get.info "Sc4ry : Circuit #{options[:circuit]} : status #{data[:status]}"
-      end
-
       def Circuits.control(options={})
         data = @@circuits_store.get key: options[:circuit]
         data[:status] = {:general => :closed, :failure_count => 0, :overtime_count => 0, :timeout_count => 0} unless data.include? :status
@@ -353,26 +434,22 @@ module Sc4ry
             worst_status.push :open
           end
         end
+        save = data[:status][:general]
         [:closed,:half_open,:open].each do |status|
           data[:status][:general] = status if worst_status.include? status
-        end          
+        end
+        Sc4ry::Helpers.notify circuit: options[:circuit], config: data if save != data[:status][:general]          
         @@circuits_store.put key: options[:circuit], value: data
       end
 
-
   end
-
 end
-
-
 
 
 Sc4ry::Circuits.register({:circuit =>:test, :config => {:exceptions => [Errno::ECONNREFUSED], :timeout =>  true, :timeout_value => 3, :check_delay => 5 }})
 
-
 # pp Sc4ry::Circuits.list
 # pp Sc4ry::Circuits.get circuit: :test
-
 
 100.times do
   sleep 1
