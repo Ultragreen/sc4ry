5dad274c826635ef80a3ed1dc3581ba2056f102f
Romain GEORGES <romain@ultragreen.net>
2022-02-21 17:52:32 +0100

    adding mattermost

---
+++ lib/sc4ry/notifiers/mattermost.rb
@@ -0,0 +1,35 @@
+module Sc4ry
+    module Notifiers
+        
+        class Mattermost
+            
+            # send metrics to Prometheus PushGateway
+            # @return [Bool]
+            def Mattermost.notify(options = {})
+                config = Sc4ry::Notifiers.get({name: :prometheus})[:config]
+                status = options[:config][:status][:general]
+                circuit = options[:circuit]
+                status_map = {:open => 0, :half_open => 1, :closed => 2}
+                
+                uri = URI.parse("#{config[:url]}/hooks/#{config[:token]}")
+                message = "notifying for circuit #{circuit.to_s}, state : #{status.to_s}."
+                if Sc4ry::Helpers::verify_service url: config[:url] then
+                    request = Net::HTTP::Post.new(uri)
+                    request.content_type = "application/json"
+                    req_options = {
+                        use_ssl: @uri.scheme == "https",
+                    }
+                    payload = { "text" => message, "username" => "Sc4ry" }
+                    Sc4ry::Helpers.log level: :debug, message: "Mattermost Notifier : #{message}"
+                    request.body = JSON.dump(payload)
+                    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+                        http.request(request)
+                    end
+                    
+                else
+                    Sc4ry::Helpers.log level: :warn, message: "Mattermost Notifier : can't notify Mattermost not reachable."
+                end
+            end
+        end
+    end
+end
